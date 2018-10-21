0629149debda5d7a698f445d0a26959c68ad4dd4
jose castelo <jccb.15@gmail.com>
2018-10-21 15:35:32 -0500

    prueba con datos generados aleatorimente

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import ARTMAP as am
 
 red = am.RedArtmap()
-matriz = [[.7, .7, 1], [.3, .8, 1], [.9, .9, 0], [.7, .9, 0], [.1, .3, 1]]
-datos = am.DatosEntrenamiento()
 
-for dato in matriz:
-    datos.ingresarDatoManual(dato[0], dato[1], dato[2])
+#-------------Datos Ejemplos Libro------------------
+#matrizLibro = [[.7, .7, 1], [.3, .8, 1], [.9, .9, 0], [.7, .9, 0], [.1, .3, 1]]
+datos = am.DatosEntrenamiento()
+#for dato in matriz:
+#    datos.ingresarDatoManual(dato[0], dato[1], dato[2])
+#--------------------------------------------------
+
+datos.generarDatosEntrenamiento(1000)
 
 red.entrenar(datos.matrizDatos)
 print(red.predecir(.9, .75))
+print(red.predecir(.4, .5))
